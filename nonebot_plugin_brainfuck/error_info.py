cae5e74212b10311bb83a24792a059eee9a587cd
Campanula <campanulamediuml@gmail.com>
2023-04-23 19:53:24 +0800

    修改错误信息

--- nonebot_plugin_brainfuck/error_info.py
+++ nonebot_plugin_brainfuck/error_info.py
@@ -3,5 +3,5 @@
 # Author     ：Campanula 梦芸 何
 POINT_CANNOT_MOVE = '指针无法移动'
 TAPE_LIMIT = '纸带长度已达到'
-INVALID_COMMAND = '打孔纸带不允许使用'
+INVALID_COMMAND = '打孔中包含错误指令'
 INBOX_ERR = '输入数据错误'
--- test_file.py
+++ test_file.py
@@ -1,5 +1,25 @@
 import nonebot_plugin_brainfuck
 
-if __name__ == '__main__':
-    r = nonebot_plugin_brainfuck.call(',>,+++++++++.<.:a,b')
+def test_normal_input():
+    r = nonebot_plugin_brainfuck.call(',>,+++++++++.<.:ab')
     print(r)
+
+def test_normal_noinput():
+    r = nonebot_plugin_brainfuck.call('>++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.')
+    # Hello, World!
+    print(r)
+
+def test_err_command():
+    r = nonebot_plugin_brainfuck.call(',>,++++++++a+.<.:ab')
+    # 打孔中包含错误指令
+    print(r)
+
+def test_err_inbox():
+    r = nonebot_plugin_brainfuck.call(',>,+++++++++.<.:a')
+    # 输入数据错误
+    print(r)
+
+if __name__ == '__main__':
+    test_err_command()
+    test_err_inbox()
+    test_normal_noinput()
