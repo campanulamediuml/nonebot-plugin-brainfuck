b7ae68d3509c7549836007eca28fd43db07780f8
Campanula <campanulamediuml@gmail.com>
2023-04-23 11:35:02 +0800

    提交pypi

---
+++ setup.py
@@ -0,0 +1,38 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+# Author     ：Campanula 梦芸 何
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+
+def read_file(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
+
+
+setup(
+        # 以下为必需参数
+        name='nonebot-plugin-brainfuck',  # 模块名
+        version='1.0.0',  # 当前版本
+        description='a brainfuck interpreter for nonebot',
+        # 简短描述
+        license='MIT',
+        long_description=read_file("README.md"),
+        author='campanula',
+        author_email='campanulamediuml@gmail.com',
+        platforms='any',
+        keywords="brainfuck",
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+        ],
+        url='https://github.com/campanulamediuml/nonebot-plugin-brainfuck',
+        install_requires=[],
+        include_package_data=True,
+        zip_safe=True,
+        packages=find_packages(),
+        python_requires='>=3.6',
+    )
